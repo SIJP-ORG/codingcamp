9ba46671fa770673fcd5f8dd0252a0e43bc39d99
Yoshi Watanabe <watanabe.tsuyoshi@noreply.github.com>
2016-01-24 14:20:17 -0800

    use more function for template.

--- greetingcard/template/scripts.js
+++ greetingcard/template/scripts.js
@@ -1,51 +1,64 @@
-window.addEventListener('load', main);
+var Constants = {
+  CARD_WIDTH: '640px',
+  CARD_HEIGHT: '480px'
+};
 
-function main() {
-  document.body.style.position = 'relative';
-  //document.body.style.backgroundColor = "#000000";
-  initCanvas();
-  initMessage();
-  addRibbon();
+function initCard() {
+  var crd = document.getElementById('card');
+  crd.style.position = 'absolute';
+  crd.style.left = '50%';
+  crd.style.width = Constants.CARD_WIDTH;
+  crd.style.height = Constants.CARD_HEIGHT;
+  crd.style.marginLeft = parseInt(Constants.CARD_WIDTH, 10) / 2 * -1 + 'px';
+  crd.style.boxShadow = '10px 10px 5px #888888';
 }
 
 function initCanvas() {
-  var elm = document.getElementById('cvs');
-  elm.style.position = "absolute";
-  elm.style.backgroundColor = "white";
-  elm.style.background = 'url(http://www.planet-aye.co.uk/seasonal05/snow.png)';
-  elm.style.top = "0px";
-  elm.style.left = "0px";
-  elm.style.width = "640px";
-  elm.style.height = "480px";
-  elm.style.zIndex = "1";
-  elm.style.pointerEvents = "none";
+  var cnv = document.getElementById('canvas');
+  cnv.style.position = "absolute";
+  cnv.style.background = 'url(http://www.pl:et-aye.co.uk/seasonal05/snow.png)';
+  cnv.style.width = Constants.CARD_WIDTH;
+  cnv.style.height = Constants.CARD_HEIGHT;
+  cnv.style.zIndex = "1";
+  cnv.style.pointerEvents = "none";
+
+  var ctx = cnv.getContext("2d");
+   ctx.fillStyle = '#ff0000';
+   ctx.fillRect(0, 0, 640, 80);
 }
 
-function initMessage() {
-  var elm = document.getElementById('msg');
-  elm.style.position = "absolute";
-  elm.style.fontSize = "80px";
-  elm.style.top = "180px";
-  elm.style.left = "100px";
-  elm.style.zIndex = '4';
-  elm.style.top = "180px";
+function initMainMessage() {
+  var msg = document.getElementById('mainMessage');
+  msg.style.position = "absolute";
+  msg.style.fontSize = "80px";
+  msg.style.top = "180px";
+  msg.style.left = "100px";
+  msg.style.zIndex = '4';
 
-elm.addEventListener('mousedown', function() {
+  msg.addEventListener('mousedown', function() {
     alert('merssage is hit')
   });
 }
 
 function addRibbon() {
-  var elm = document.createElement('img');
-  elm.style.position = "relative";
-  document.body.appendChild(elm);
-  elm.style.top = '120px';
-  elm.style.left = '200px'
-  elm.style.backgroundColor = "#000000";
-  elm.style.height = 100;
-  elm.style.zIndex = '9';
-  elm.src = 'http://pngimg.com/upload/ribbon_PNG1557.png';
-  elm.addEventListener('mousedown', function() {
+  var img = document.createElement('img');
+  document.getElementById('card').appendChild(img);
+  img.style.position = "absolute";
+  img.style.top = '110px';
+  img.style.left = '0px'
+  img.style.height = 100;
+  img.style.zIndex = '9';
+  img.src = 'http://pngimg.com/upload/ribbon_PNG1557.png';
+  img.addEventListener('mousedown', function() {
     alert('hi')
   });
 }
+
+function main() {
+  initCard();
+  initCanvas();
+  initMainMessage();
+  addRibbon();
+}
+
+window.addEventListener('load', main);
