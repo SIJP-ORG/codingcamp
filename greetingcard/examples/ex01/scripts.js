522abce7fd0cbd3c7cbaac828d9e343aad11dd4c
Yoshi Watanabe <watanabe.tsuyoshi@noreply.github.com>
2016-01-31 18:26:07 -0800

    changed all single quotes to double quotes.

--- greetingcard/examples/ex01/scripts.js
+++ greetingcard/examples/ex01/scripts.js
@@ -1,34 +1,34 @@
 
 function setupCard() {
-  var el = document.getElementById('card');
-  el.style.background = 'url(nc114407.jpg)';
-  el.style.position = 'absolute';
-  el.style.top = '5%';
-  el.style.left = '50%';
-  el.style.height = '480px';
-  el.style.width = '640px';
-  el.style.marginLeft = 640 / 2 * -1 + 'px';
-  el.style.boxShadow = '10px 10px 5px #888888';
+  var el = document.getElementById("card");
+  el.style.background = "url(nc114407.jpg)";
+  el.style.position = "absolute";
+  el.style.top = "5%";
+  el.style.left = "50%";
+  el.style.height = "480px";
+  el.style.width = "640px";
+  el.style.marginLeft = 640 / 2 * -1 + "px";
+  el.style.boxShadow = "10px 10px 5px #888888";
 }
 
 function setupCanvas() {
-  var el = document.getElementById('canvas');
+  var el = document.getElementById("canvas");
   el.style.position = "absolute";
-  el.style.top = '0px';
-  el.style.left = '0px';
-  el.style.height = '480px';
-  el.style.width = '640px';
+  el.style.top = "0px";
+  el.style.left = "0px";
+  el.style.height = "480px";
+  el.style.width = "640px";
 }
 
 function drawShapes() {
-  var cx = document.getElementById('canvas').getContext("2d");
-  cx.fillStyle = '#ff0000';
+  var cx = document.getElementById("canvas").getContext("2d");
+  cx.fillStyle = "#ff0000";
   cx.fillRect(0, 0, 640, 80);
 }
 
 function setupGreeting() {
-  var gr = document.getElementById('greeting');
-  gr.textContent = 'あけましておめでとうございます!';
+  var gr = document.getElementById("greeting");
+  gr.textContent = "あけましておめでとうございます!";
   gr.style.position = "absolute";
   gr.style.top = "70px";
   gr.style.left = "70px";
@@ -36,30 +36,30 @@ function setupGreeting() {
 }
 
 function setupAction() {
-  var msg = document.createElement('div');
-  msg.textContent = '今年もよろしくお願いします';
-  msg.style.position = 'absolute';
-  msg.style.top = '-200px'
-  msg.style.left = '140px';
-  msg.style.fontSize = '30px';
-  msg.id = 'message';
-  document.getElementById('card').appendChild(msg);
+  var msg = document.createElement("div");
+  msg.textContent = "今年もよろしくお願いします";
+  msg.style.position = "absolute";
+  msg.style.top = "-200px"
+  msg.style.left = "140px";
+  msg.style.fontSize = "30px";
+  msg.id = "message";
+  document.getElementById("card").appendChild(msg);
 
-  var icon = document.createElement('img');
-  icon.src = 'nenga_mark18_kadomatsu.png';
+  var icon = document.createElement("img");
+  icon.src = "nenga_mark18_kadomatsu.png";
   icon.style.position = "absolute";
-  icon.style.top = '130px';
-  icon.style.left = '260px'
+  icon.style.top = "130px";
+  icon.style.left = "260px"
   icon.style.height = 100;
-  icon.addEventListener('mousedown', effect);
-  document.getElementById('card').appendChild(icon);
+  icon.addEventListener("mousedown", effect);
+  document.getElementById("card").appendChild(icon);
 
-  var hint = document.createElement('p');
-  hint.textContent = '門松をクリックしてね';
-  hint.style.position = 'absolute';
-  hint.style.top = '210px';
-  hint.style.left = '250';
-  document.getElementById('card').appendChild(hint);
+  var hint = document.createElement("p");
+  hint.textContent = "門松をクリックしてね";
+  hint.style.position = "absolute";
+  hint.style.top = "210px";
+  hint.style.left = "250";
+  document.getElementById("card").appendChild(hint);
 }
 
 function effect() {
@@ -71,7 +71,7 @@ function effect() {
             clearInterval(id);
         } else {
             pos++;
-            el.style.top = pos + 'px';
+            el.style.top = pos + "px";
         }
     }
 }
@@ -84,4 +84,4 @@ function main() {
   setupAction();
 }
 
-window.addEventListener('load', main);
+window.addEventListener("load", main);
--- greetingcard/examples/ex02/scripts.js
+++ greetingcard/examples/ex02/scripts.js
@@ -1,83 +1,83 @@
 var Constants = {
-  CARD_WIDTH: '640px',
-  CARD_HEIGHT: '480px'
+  CARD_WIDTH: "640px",
+  CARD_HEIGHT: "480px"
 };
 
 function initCard() {
-  var crd = document.getElementById('card');
-  crd.style.position = 'absolute';
-  crd.style.left = '50%';
+  var crd = document.getElementById("card");
+  crd.style.position = "absolute";
+  crd.style.left = "50%";
   crd.style.width = Constants.CARD_WIDTH;
   crd.style.height = Constants.CARD_HEIGHT;
-  crd.style.marginLeft = parseInt(Constants.CARD_WIDTH, 10) / 2 * -1 + 'px';
-  crd.style.boxShadow = '10px 10px 5px #888888';
+  crd.style.marginLeft = parseInt(Constants.CARD_WIDTH, 10) / 2 * -1 + "px";
+  crd.style.boxShadow = "10px 10px 5px #888888";
 }
 
 function initCanvas() {
-  var cnv = document.getElementById('canvas');
+  var cnv = document.getElementById("canvas");
   cnv.style.position = "absolute";
-  cnv.style.background = 'url(http://www.pl:et-aye.co.uk/seasonal05/snow.png)';
+  cnv.style.background = "url(http://www.pl:et-aye.co.uk/seasonal05/snow.png)";
   cnv.style.width = Constants.CARD_WIDTH;
   cnv.style.height = Constants.CARD_HEIGHT;
   cnv.style.zIndex = "1";
   cnv.style.pointerEvents = "none";
 
   var ctx = cnv.getContext("2d");
-  //ctx.fillStyle = '#0000ff';
+  //ctx.fillStyle = "#0000ff";
   //ctx.fillRect(0, 80, 640, 80);
 }
 
 function initMainMessage() {
-  // var msg = document.getElementById('mainMessage');
-  // msg.innerHTML = 'Merry Chrismas';
+  // var msg = document.getElementById("mainMessage");
+  // msg.innerHTML = "Merry Chrismas";
   // msg.style.position = "absolute";
   // msg.style.fontSize = "40px";
   // msg.style.top = "70px";
   // msg.style.left = "70px";
-  // msg.style.zIndex = '4';
+  // msg.style.zIndex = "4";
   //
-  // msg.addEventListener('mousedown', function() {
-  //   alert('merssage is hit')
+  // msg.addEventListener("mousedown", function() {
+  //   alert("merssage is hit")
   // });
 }
 
 function addAction() {
-  var img = document.createElement('img');
-  document.getElementById('card').appendChild(img);
+  var img = document.createElement("img");
+  document.getElementById("card").appendChild(img);
   img.style.position = "absolute";
-  img.style.top = '230px';
-  img.style.left = '30px'
+  img.style.top = "230px";
+  img.style.left = "30px"
   img.style.height = 200;
-  img.style.zIndex = '9';
-  img.src = 'christmas_usagi.png';
-  img.addEventListener('mousedown', effect);
+  img.style.zIndex = "9";
+  img.src = "christmas_usagi.png";
+  img.addEventListener("mousedown", effect);
 
-  var box = document.createElement('img');
-  document.getElementById('card').appendChild(box);
-  box.style.position = 'absolute';
-  box.src = 'fukidashi_bw03.png';
-  box.style.top = '20px';
-  box.style.left = '120px';
-  box.style.height = '400';
-  box.style.zIndex = '9';
+  var box = document.createElement("img");
+  document.getElementById("card").appendChild(box);
+  box.style.position = "absolute";
+  box.src = "fukidashi_bw03.png";
+  box.style.top = "20px";
+  box.style.left = "120px";
+  box.style.height = "400";
+  box.style.zIndex = "9";
 
-  var inst = document.createElement('p');
-  document.getElementById('card').appendChild(inst);
-  inst.style.position = 'absolute';
-  inst.style.top = '410px';
-  inst.style.left = '50';
-  inst.style.zIndex = '9';
-  inst.innerHTML = 'サンタさんをクリックしてね';
+  var inst = document.createElement("p");
+  document.getElementById("card").appendChild(inst);
+  inst.style.position = "absolute";
+  inst.style.top = "410px";
+  inst.style.left = "50";
+  inst.style.zIndex = "9";
+  inst.innerHTML = "サンタさんをクリックしてね";
 
-  var para = document.createElement('div');
-  para.id = 'para';
-  para.innerHTML = 'Merry Christmas!';
-  document.getElementById('card').appendChild(para);
-  para.style.position = 'absolute';
-  para.style.top = '170';
-  para.style.fontSize = '50px';
-  para.style.left = '190px';
-  para.style.zIndex = '9';
+  var para = document.createElement("div");
+  para.id = "para";
+  para.innerHTML = "Merry Christmas!";
+  document.getElementById("card").appendChild(para);
+  para.style.position = "absolute";
+  para.style.top = "170";
+  para.style.fontSize = "50px";
+  para.style.left = "190px";
+  para.style.zIndex = "9";
 }
 
 function effect() {
@@ -103,8 +103,8 @@ function effect() {
             }
             //fsize = larger ? fsize++ : fsize--;
             elem.style.fontSize = fsize + "px";
-            //elem.style.top = pos + 'px';
-            //elem.style.left = pos + 'px';
+            //elem.style.top = pos + "px";
+            //elem.style.left = pos + "px";
         }
     }
 }
@@ -116,4 +116,4 @@ function main() {
   addAction();
 }
 
-window.addEventListener('load', main);
+window.addEventListener("load", main);
