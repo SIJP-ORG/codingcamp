15e8c4c0594d894e0c1c582e42394d8314721e2b
Yoshi Watanabe <watanabe.tsuyoshi@noreply.github.com>
2016-06-18 12:25:48 -0700

    adding PDF files

--- greetingcard/myproject/README.md
+++ greetingcard/myproject/README.md
@@ -1,4 +1,9 @@
-#step11
+#Step06
 
-[FIXED] Need to define a function for a common operation.
-[FIXED]Need to re-use the code for the common operation using the function.
+[FIXED] The main message contains a spelling error.　
+[FIXED] The main message's location is not set at a right location.
+[FIXED] The name of the Santa's image file is incorrect.
+[FIXED] The card's outline needs shadow.
+[FIXED] The interactive message is in Japanese when the language is set to English.
+[FIXED] The number of "snow flake" image is one for each click. Needs to show more.
+[FIXED] The message only includes grand-pa, grand-ma, and aunt. It should also show uncle.
--- greetingcard/myproject/scripts.js
+++ greetingcard/myproject/scripts.js
@@ -1,82 +1,77 @@
-(function(SIJPCC) {
+(function(SIJPCC){
+
+  var clickCount = 0;
+  var persons = ['おじいちゃん', 'おばあちゃん','おばさん','おじさん'];
 
   function setupCard() {
     var el = document.getElementById("card");
     el.style.background = "url(001-Basik-Paper.png)";
-    setLayout(el, "50%", "5%", 640, 480);
+    el.style.position = "absolute";
+    el.style.left = "50%";
+    el.style.top = "5%";
+    el.style.width = 640;
+    el.style.height = 480;
     el.style.marginLeft = 640 / 2 * -1;
     el.style.boxShadow = "10px 10px 5px #888888";
   }
 
   function setupGreeting() {
     var el = document.getElementById("greeting");
-    setLayout(el, 100, 120, -1, -1)
+    el.style.position = "absolute";
+    el.style.left = 100;
+    el.style.top = 120;
     el.style.fontSize = 60;
-
-    if (window.navigator.languages[0] == "ja") {
-      el.textContent = "メリークリスマス！";
-    } else {
-      el.textContent = "Merry Christmas!";
-    }
+    el.textContent = "メリークリスマス！";
   }
 
   function setupMessage() {
     var el = document.createElement("p");
     document.getElementById("card").appendChild(el);
-    setLayout(el, 170, 270, 390, 100);
+    el.style.position = "absolute";
+    el.style.left = 170;
+    el.style.top = 270;
+    el.style.width = 390;
+    el.style.height = 100;
     el.style.fontSize = 24;
     el.id = "msg";
-
-    if (window.navigator.languages[0] == "ja") {
-      el.textContent = "⇦アイコンをクリックしてね";
-    } else {
-      el.textContent = "⇦Please click the icon";
-    }
+    el.textContent = "⇦アイコンをクリックしてね";
   }
 
   function setupIcon() {
     var el = document.createElement("img");
     document.getElementById("card").appendChild(el);
-    el.src = "christmas_mark01_santa.png";
-    setLayout(el, 80, 260, -1, 100);
+    el.src = "santa_claus.png";
+    el.style.position = "absolute";
+    el.style.left = 80;
+    el.style.top = 260;
+    el.style.height = 100;
 
     el.addEventListener("mousedown", function() {
+      var to = "";
+      for (var i = 0; i < persons.length; i++) {
+        to += persons[i] + " ";
+      }
+
       var msg = document.getElementById("msg");
-      if (window.navigator.languages[0] == "ja") {
-        msg.textContent = "ステキなクリスマスになりますように！";
+      msg.textContent = to + "楽しいクリスマスになりますように！";
+
+      var im = document.createElement("img");
+      document.getElementById("card").appendChild(im);
+      var x = Math.floor((Math.random() * 640) + 1);
+      var y = Math.floor((Math.random() * 480) + 1);
+      im.style.position = "absolute";
+      im.style.left = x;
+      im.style.top = y;
+      im.style.width = 30;
+      im.style.height = 30;
+      if (clickCount < 10) {
+        im.src = "snow_crystal2.png";
       } else {
-        msg.textContent = "Have a nice Christmas!";
+        im.src = "snow_crystal3.png"
       }
 
-      for (var i = 0; i < 10; i++) {
-        var x = Math.floor((Math.random() * 640) + 1);
-        var y = Math.floor((Math.random() * 480) + 1);
-        var el = document.createElement("img");
-        document.getElementById("card").appendChild(el);
-        setLayout(el, x, y, 30, 30);
-        el.src = "snow_crystal3.png";
-      }
+      clickCount = clickCount + 1;
     });
-  }
-
-  function setLayout(element, x, y, width, height) {
-    element.style.position = "absolute";
-    element.style.left = x;
-    element.style.top = y;
-
-    if (width !== -1) {
-      element.style.width = width;
-    }
-    if (height !== -1) {
-      element.style.height = height;
-    }
-  }
-
-  function setupCornerImage() {
-    var el = document.createElement("img");
-    document.getElementById("card").appendChild(el);
-    setLayout(el, 470, 20, 150, 150);
-    el.src = "corner06_dots.png";
   }
 
   SIJPCC.main = function() {
@@ -84,7 +79,6 @@
     setupGreeting();
     setupMessage();
     setupIcon();
-    //setupCornerImage();
   }
 
 }(window.SIJPCC = window.SIJPCC || {}));
